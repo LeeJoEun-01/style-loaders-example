eb8f2e81e1a83c3f2e151d2a252434ff38a82d4b
LeeJoEun-01 <joeun1005@naver.com>
2022-03-21 10:28:40 +0900

    Create Project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+// import "./App.scss";
+import styles from "./App.module.css";
+
+import Button from './components/Button'
+
+console.log(styles);
+// { 형식: (우리가 사용하는)실제 key:[filename]_[classname]__[hash]
+//   App: "App_App__Vwa6h" 
+//   App-header: "App_App-header__sA7RO"
+//   App-link: "App_App-link__gOwq-"
+//   App-logo: "App_App-logo__ABdGL"
+//   App-logo-spin: "App_App-logo-spin__q5hBl"
+// }
+
+// module.css파일을 import하면
+// classname이 App으로 들어가면 안되고 js의 표현식이 들어가야 한다!!!!!!!  ==> 오염되지 않게 사용
+// 1. className={styles.App}    또는   2. className={styles["App"]} 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={styles["App"]}>
+      <header className={styles["header"]}>
+        <img src={logo} className={styles["logo"]} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button>Button</Button>
       </header>
     </div>
   );
